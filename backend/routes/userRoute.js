196d8ea29170a4435010b79af59aff3ee2befdce
Dawit Solomon <dawit8908@gmail.com>
2025-09-30 17:36:54 +0300

    Login and Signin API endpoints are done!

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -2,10 +2,13 @@ const express = require("express");
 const router = express.Router();
 const multer = require("multer");
 const userController = require("./../controllers/userController");
-
+const authController = require("./../controllers/authController");
 // use memory storage so files are available in req.file.buffer and not saved to disk
 const storage = multer.memoryStorage();
 const upload = multer({ storage });
+
+router.route("/signup").post(authController.signup);
+router.route("/login").post(authController.login);
 
 router
   .route("/")
