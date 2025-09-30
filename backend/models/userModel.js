458f0400362f1c2c3ac9a9428417fbc96c40db4c
Dawit Solomon <dawit8908@gmail.com>
2025-09-30 17:36:54 +0300

    Login and Signin API endpoints are done!

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,4 +1,6 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const validator = require("validator");
 
 const userSchema = new mongoose.Schema({
   fullName: {
@@ -8,14 +10,35 @@ const userSchema = new mongoose.Schema({
   email: {
     type: String,
     required: [true, "Email is Required"],
+    unique: true,
+
+    lowercase: true,
+    validate: [validator.isEmail, "Please provide email!"],
   },
   username: {
     type: String,
     required: [true, "username is Required!"],
+    unique: true,
+    tolower: true,
+  },
+  password: {
+    type: String,
+    required: [true, "Please provide password!"],
+    minlength: 8,
+    select: false,
+  },
+  passwordConfirm: {
+    type: String,
+    required: [true, "Please confirm password!"],
+    validate: {
+      validator: function (el) {
+        return el === this.password;
+      },
+      message: "Password is not the same!",
+    },
   },
   role: {
     type: String,
-    required: [true, "Role is Required!"],
   },
   status: {
     type: String,
@@ -28,6 +51,21 @@ const userSchema = new mongoose.Schema({
     select: false,
   },
 });
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+
+  this.password = await bcrypt.hash(this.password, 12);
+  this.passwordConfirm = undefined;
+  next();
+});
+
+userSchema.methods.correctPassword = async function (
+  candidatePassword,
+  userPassword
+) {
+  return await bcrypt.compare(candidatePassword, userPassword);
+};
 
 const UserModel = mongoose.model("User", userSchema);
 
--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+const User = require("./models/userModel.js");
 
 dotenv.config({ path: "./config.env" });
 
@@ -10,7 +11,12 @@ const DB = process.env.DATABASE_URL;
 
 mongoose
   .connect(DB)
-  .then(() => console.log("Database Connected Successfully"))
+  .then(() => {
+    console.log("Database Connected Successfully");
+    User.init()
+      .then(() => console.log("Indexes are ready ✅"))
+      .catch((err) => console.error("Index error:", err));
+  })
   .catch((err) => console.log("Database is not Connected", err));
 
 app.listen(port, () => console.log(`Server is Running on port ${port}`));
